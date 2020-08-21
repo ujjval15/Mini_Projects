dc52b4c906bf1fb051541ef8992d967931ed16c6
Ujjaval Surti <53287904+ujjval15@users.noreply.github.com>
2020-08-21 20:39:05 +0530

    Add files via upload
    The task is to read the news using python.I create a program that will give you daily top 10 latest news. i used  https://newsapi.org/ which gives the news API.

---
+++ newsTeller.py
@@ -0,0 +1,15 @@
+def speak(str):
+    from win32com.client import Dispatch
+    speak=Dispatch("SAPI.spVoice")
+    speak.Speak(str)
+
+if __name__ == '__main__':
+    import requests
+    import json
+    url = ('https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=b965b91ab1ea44a1ac91832ae6b9ff83')
+
+    response = requests.get(url)
+    text = response.text
+    my_json = json.loads(text)
+    for i in range(0, 11):
+        speak(my_json['articles'][i]['description'])
